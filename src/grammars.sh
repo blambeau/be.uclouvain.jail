5d8615b2de3546b25626db50206b7135c45d5565
Bernard Lambeau <blambeau@gmail.com>
2008-01-10 17:18:44 +0000

    + Jail grammar version 2.
    git-svn-id: http://svn.chefbe.net/be.uclouvain.jail/trunk@56 bb353e37-10b9-4b19-88b1-c35c3065af8f

--- src/grammars.sh
+++ src/grammars.sh
@@ -1,7 +1,11 @@
 export CLASSPATH=./algo:./dialect:./core
+
+autogram -t agast -l java -o core/be/uclouvain/jail/vm/autogram/v2 core/be/uclouvain/jail/vm/autogram/v2/JailUtils.ag
+autogram -t agast -l java -o core/be/uclouvain/jail/vm/autogram/v2 core/be/uclouvain/jail/vm/autogram/v2/Jail.ag
+
+
 #autogram -t agpeg -l java -o dialect/be/uclouvain/jail/dialect/utils dialect/be/uclouvain/jail/dialect/utils/UTILS.ag
 #autogram -t agast -l java -o dialect/be/uclouvain/jail/dialect/dot dialect/be/uclouvain/jail/dialect/dot/DOT.ag
 #autogram -t agast -l java -o dialect/be/uclouvain/jail/dialect/seqp dialect/be/uclouvain/jail/dialect/seqp/SEQP.ag
-autogram -t agast -l java -o dialect/be/uclouvain/jail/dialect/upv2 dialect/be/uclouvain/jail/dialect/upv2/UPV2.ag
-#autogram -t agast -l java -o core/be/uclouvain/jail/vm/autogram core/be/uclouvain/jail/vm/autogram/Jail.ag
+#autogram -t agast -l java -o dialect/be/uclouvain/jail/dialect/upv2 dialect/be/uclouvain/jail/dialect/upv2/UPV2.ag
 #autogram -t agast -l java -o algo/be/uclouvain/jail/algo/graph/copy/match algo/be/uclouvain/jail/algo/graph/copy/match/GMatch.ag
