172b2d8aa9a285cbe2e23a4ecf58b07f1b1ad4a3
Bernard Lambeau <blambeau@gmail.com>
2008-01-18 18:47:14 +0000

    + Dist added to repository.
    git-svn-id: http://svn.chefbe.net/be.uclouvain.jail/trunk@66 bb353e37-10b9-4b19-88b1-c35c3065af8f

---
+++ dist/jail/jail.sh
@@ -0,0 +1,8 @@
+export AWT_TOOLKIT=MToolkit
+JAIL_HOME=/home/blambeau/work/development/java/be.uclouvain.jail/dist/jail
+export CLASSPATH=.:$JAIL_HOME/lib
+for i in $JAIL_HOME/lib/*.jar 
+do
+    export CLASSPATH=$CLASSPATH:$i
+done
+java -Xmx512m -Xms512m -ea be.uclouvain.jail.Jail $*					 
