8f7154ef8a0b82694419ebb92a3bb1b37951ed06
Bernard Lambeau <blambeau@gmail.com>
2011-04-19 13:21:11 +0000

    Added command line tools
    git-svn-id: http://svn.chefbe.net/be.uclouvain.jail/trunk@106 bb353e37-10b9-4b19-88b1-c35c3065af8f

---
+++ lsmi.sh
@@ -0,0 +1,2 @@
+export CLASSPATH=$CLASSPATH:dist/jail/lib/be.uclouvain.jail.jar:dist/jail/lib/commons-beanutils.jar:dist/jail/lib/commons-collections-3.1.jar:dist/jail/lib/commons-logging.jar:dist/jail/lib/grappa.jar:dist/jail/lib/jline-0.9.91.jar:dist/jail/lib/net.chefbe.autogram.jar:dist/jail/lib/net.chefbe.javautils.jar
+java be.uclouvain.jail.contribs.lsmi.LSMIGenerateDFA $*
