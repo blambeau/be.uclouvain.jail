1809d28965f63bb4963473f273079c01e1e5f32d
LAMBEAU Bernard <blambeau@cassidy.(none)>
2011-04-19 16:06:19 +0200

    Make jail.sh independent of absolute paths
    Conflicts:
    
    	dist/jail/jail.sh

--- dist/jail/jail.sh
+++ dist/jail/jail.sh
@@ -1,5 +1,9 @@
 export AWT_TOOLKIT=MToolkit
+<<<<<<< HEAD:dist/jail/jail.sh
 JAIL_HOME=/home/blambeau/work/development/java/be.uclouvain.jail/dist/jail
+=======
+JAIL_HOME=`pwd` 
+>>>>>>> 1abfcb6... Make jail.sh independent of absolute paths:dist/jail/jail.sh
 export CLASSPATH=.:$JAIL_HOME/lib
 for i in $JAIL_HOME/lib/*.jar 
 do
